dcdbbb7b2a5ada2eea258b8ef55b3850617e5344
idsc3101 <idsc3101@gmail.com>
2013-10-07 15:07:48 -0500

    WorkDay5

--- RightTriangle/RightTriangle/Form1.vb
+++ RightTriangle/RightTriangle/Form1.vb
@@ -3,9 +3,18 @@ Public Class Form1
 
 
     Private Sub btnCalculate_Click(sender As System.Object, e As System.EventArgs) Handles btnCalculate.Click
+        Dim a As Double = CDbl(txtSideOne.Text)
+        Dim b As Double = CDbl(txtSideTwo.Text)
+        Dim x As Double
 
-        txtHyp.Text = "Not Implemented Yet"
+        x = Hypotenuse(a, b)
+        txtHyp.Text = CStr(x)
 
     End Sub
 
+    Function Hypotenuse(ByVal a As Double, ByVal b As Double) As Double
+        Dim c As Double
+        c = Math.Sqrt(a ^ 2 + b ^ 2)
+        Return c
+    End Function
 End Class
